024ad4c72402c7d3599338cf17a4f4182d56dea9
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-08-20 17:08:20 +0300

    add de;ete user and user signout

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,6 +13,10 @@ import {
 	updateUserStart,
 	updateUserFailure,
 	updateUserSuccess,
+	deleteUserStart,
+	deleteUserSuccess,
+	deleteUserFailure,
+	signOut,
 } from '../redux/user/userSlice'
 
 const Profile = () => {
@@ -82,6 +86,31 @@ const Profile = () => {
 		}
 	}
 
+	const handleDeleteAccount = async () => {
+		try {
+			dispatch(deleteUserStart())
+			const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+				method: 'DELETE',
+			})
+			const data = await res.json()
+			if (data.success === false) {
+				dispatch(deleteUserFailure(data))
+				return
+			}
+			dispatch(deleteUserSuccess(data))
+		} catch (error) {
+			dispatch(deleteUserFailure(error))
+		}
+	}
+	const handleSignOut = async () => {
+		try {
+			await fetch(`/api/auth/signout`)
+			dispatch(signOut())
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	return (
 		<div className='p-3 max-w-lg mx-auto'>
 			<h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -140,8 +169,15 @@ const Profile = () => {
 				</button>
 			</form>
 			<div className='flex justify-between mt-5'>
-				<span className='text-red-700 cursor-pointer'>Delete Account</span>
-				<span className='text-red-700 cursor-pointer'>Sign out</span>
+				<span
+					onClick={handleDeleteAccount}
+					className='text-red-700 cursor-pointer'
+				>
+					Delete Account
+				</span>
+				<span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
+					Sign out
+				</span>
 			</div>
 			<p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
 			<p className='text-green-700 mt-5'>
