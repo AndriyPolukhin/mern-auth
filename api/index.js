569aa44372e5c2331fdf67afdefc83a26919c300
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-08-20 16:09:45 +0300

    crdate update user apiroute

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
+import path from 'path'
 
 dotenv.config()
 
@@ -18,10 +20,18 @@ mongoose
 	.catch((err) => {
 		console.log(err)
 	})
+
 const app = express()
 
-app.use(express.json())
+// const __dirname = path.resolve()
+// app.use(express.static(path.join(__dirname, '/client/dist')))
 
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+// })
+
+app.use(express.json())
+app.use(cookieParser())
 app.listen(5000, () => {
 	console.log(`Server running on port 5000`)
 })
